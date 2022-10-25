3494078ff6540ea6a7fb0152474482f457000bcc
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-10-25 10:24:23 -0400

    Fixed missing starting number

--- looper.py
+++ looper.py
@@ -1,7 +1,7 @@
 import csv 
 
 location_name = input('Enter you locations: ')
-locations = location_name.split(' ')
+locations = location_name.split(' ' or ',')
 increments = input('Increment by: ')
 increments_int = int(increments)
 answer = []
@@ -9,15 +9,22 @@ for i in range(len(locations)):
     single = locations[i]
     name_split = single.split("-")
     converted_num = int(name_split[3])
+    print(single)
+    f = open('test.csv', 'a', newline='')
+    writer = csv.writer(f)
+    writer.writerow([single])
     for converted_num in range(increments_int):
         num = converted_num + 1
         double_digit = f'{num:02d}'
         name_split[3] = double_digit
         answer = '-'.join(name_split)
-        with open('test.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow([answer])
+        # with open('test.csv', 'a') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow([answer])
+        writer.writerow([answer])
         print(answer)
+    f.close()
+        
 
         
 
